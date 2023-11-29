b615c4d6b15ba32087f251daae3ce29adb39e221
akoluf <Rifald478@gmail.com>
2023-11-29 11:35:33 +0300

    начало номер 1

---
+++ seminar1/ex1.java
@@ -0,0 +1,11 @@
+package seminar1;
+
+/**
+ * ex1: 1. Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+ */
+public class ex1 {
+    public static void main(String[] args) {
+
+    }
+
+}
