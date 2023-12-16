b8e69851fa33828a97052ec0c1105b93134a3210
akoluf <Rifald478@gmail.com>
2023-12-16 21:43:48 +0300

    решена задача 2

--- seminar3/ex2.java
+++ seminar3/ex2.java
@@ -1,7 +1,24 @@
 /**
- * ex2
+Дан произвольный массив целых чисел. Создайте список ArrayList, заполненный данными из этого массива. Необходимо удалить из списка четные числа и вернуть результирующий.
+исходный массив - [1, 2, 3, 4, 5, 6, 7, 8, 9]
  */
+import java.util.ArrayList;
+
 public class ex2 {
+    public static void main(String[] args) {
+        // Исходный массив
+        int[] originalArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+        // Создание ArrayList из массива
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i : originalArray) {
+            arrayList.add(i);
+        }
 
+        // Удаление четных чисел
+        arrayList.removeIf(n -> (n % 2 == 0));
+
+        // Вывод результата
+        System.out.println(arrayList);
+    }
 }
